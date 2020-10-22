79221eabc3eaecc59d0f9aab8ff7b78de098e528
Erich Barnstedt <erichb@microsoft.com>
2020-10-22 23:40:11 +0200

    small cleanup.

--- opcpublisher/EncryptedNetworkCredential.cs
+++ opcpublisher/EncryptedNetworkCredential.cs
@@ -3,7 +3,6 @@
 //  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
 // ------------------------------------------------------------
 
-using OpcPublisher.Configurations;
 using System;
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
